6eb62360436fe6610679ffa19ec13b325ec2c946
daniel-j <daniel.j.hede@gmail.com>
2023-06-26 01:45:46 +0200

    remove old paths

--- src/pimoroni_pico/drivers/bme68x.nim
+++ src/pimoroni_pico/drivers/bme68x.nim
@@ -15,19 +15,6 @@ importc:
 
   sysPath armSysrootInclude
   sysPath armInstallInclude
-#   sysPath picoSdkPath / "src/rp2040/hardware_regs/include"
-#   sysPath picoSdkPath / "lib/lwip/contrib/ports/freertos/include"
-#   sysPath picoSdkPath / "src/common/pico_base/include"
-#   sysPath picoSdkPath / "src/rp2_common/pico_platform/include"
-#   sysPath picoSdkPath / "src/rp2_common/pico_rand/include"
-#   sysPath picoSdkPath / "src/rp2_common/pico_cyw43_driver/include"
-#   sysPath cmakeBinaryDir / "generated/pico_base"
-#   path picoSdkPath / "lib/mbedtls/include"
-#   path picoSdkPath / "lib/mbedtls/library"
-#   path picoSdkPath / "src/rp2_common/pico_lwip/include"
-#   path picoSdkPath / "lib/lwip/src/include"
-#   path cmakeSourceDir
-#   path getProjectPath()
   path bme68xInclude
 
   renameCallback futharkRenameCallback
